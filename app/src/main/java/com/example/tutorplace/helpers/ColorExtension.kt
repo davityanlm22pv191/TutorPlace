510c3a99d49f06937aed50418fe4083d7f82b7f9
leva <freedomlove6574@gmail.com>
2025-08-30 19:41:48 +0300

    implement dynamic system bar color based on screen background + create ColorExtension.kt with isLight() method

---
+++ app/src/main/java/com/example/tutorplace/helpers/ColorExtension.kt
@@ -0,0 +1,19 @@
+package com.example.tutorplace.helpers
+
+import androidx.compose.ui.graphics.Color
+import kotlin.math.sqrt
+
+fun Color.isLight(): Boolean {
+	val r = (red * 255).toInt()
+	val g = (green * 255).toInt()
+	val b = (blue * 255).toInt()
+
+	// perceived brightness algorithm
+	val brightness = sqrt(
+		r * r * 0.241 +
+				g * g * 0.691 +
+				b * b * 0.068
+	)
+
+	return brightness >= 130
+}
--- app/src/main/java/com/example/tutorplace/ui/activity/main/MainActivity.kt
+++ app/src/main/java/com/example/tutorplace/ui/activity/main/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.tutorplace.ui.activity.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.SideEffect
+import androidx.core.view.WindowInsetsControllerCompat
+import com.example.tutorplace.helpers.isLight
 import com.example.tutorplace.ui.navigation.AppNavigationGraph
+import com.example.tutorplace.ui.theme.ScreenColor
 import com.example.tutorplace.ui.theme.TutorPlaceTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,20 +17,21 @@ class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		val startDestination = intent.getStringExtra("start_destination") ?: "auth"
-		enableEdgeToEdge(
-			statusBarStyle = SystemBarStyle.light(
-				android.graphics.Color.WHITE,
-				android.graphics.Color.WHITE
-			),
-			navigationBarStyle = SystemBarStyle.light(
-				android.graphics.Color.WHITE,
-				android.graphics.Color.WHITE
-			)
-		)
+		enableEdgeToEdge()
 		setContent {
 			TutorPlaceTheme {
+				val isLightScreenColor = ScreenColor.isLight()
+				SideEffect {
+					val windowInsetsController =
+						WindowInsetsControllerCompat(window, window.decorView)
+					windowInsetsController.isAppearanceLightStatusBars = isLightScreenColor
+					windowInsetsController.isAppearanceLightNavigationBars = isLightScreenColor
+				}
+
 				AppNavigationGraph(startDestination)
 			}
 		}
 	}
 }
+
+
